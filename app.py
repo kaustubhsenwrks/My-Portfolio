9117a1a3427aba14bb121f5b92a4a807495b55d2
Kaustubh Sen <kaustubhsen2707@gmail.com>
2025-02-20 12:24:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route("/upcoming")
+def upcoming():
+    return render_template("upcoming.html")
+
 if __name__ == "__main__":
     app.run(debug =True)
